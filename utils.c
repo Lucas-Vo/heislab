1ef35322d88bd53cbb5f027618d776bf36af1e57
Vetle Dahl <vetlerd@stud.ntnu.no>
2025-01-31 16:46:48 +0100

    Got ya

--- utils.c
+++ utils.c
@@ -6,20 +6,20 @@
 @brief Sorts the query array based on the priority bit, direction moving, and the floor bit
 @param query: pointer to the query array
 */
-uint8_t sort_query (uint8_t *query, fsm_state_t *elevator_state) {
+uint8_t sort_query (uint8_t *arr_query, fsm_state_t *p_elevator_state) {
     int i, j;
     uint8_t temp;
-    for (j = 0; j < M_QUERY_LEN; j++) {
+    for (j = 0; j < M_QUERY_LEN-1; j++) {
         for (i = 0; i < M_QUERY_LEN-j; i++) {
-            if (query[i]&0x40 > query[i+1]&0x40) {
+            if (arr_query[i]&0x40 > arr_query[i+1]&0x40) {
                  //checks if the 7th bit (Prioritized in stage bit) is bigger than no 2
                 continue;
             }
-            else if (query[i]&0x40 == 0) {
+            else if (arr_query[i]&0x40 == 0) {
                 //checks if relevant bit is 0, which means it is not prioritized this round
                 continue;
             }
-            else if (query[j+1]&0x40 == 1){
+            else if (arr_query[j+1]&0x40 == 1){
                 //If the next on the queue is prioritized, then swap if the 
 
             }
@@ -32,7 +32,17 @@ uint8_t sort_query (uint8_t *query, fsm_state_t *elevator_state) {
 @brief Prioritizes the inputs based on the priority bit, direction moving, and the floor bit
 @param query: pointer to the query array
 */
-uint8_t prioritize_inputs (uint8_t *query, fsm_state_t *elevator_state){
+uint8_t prioritize_inputs (uint8_t *arr_query, fsm_state_t *p_elevator_state){
+    for (uint8_t i = 0; i <M_QUERY_LEN; i++){
+        if (p_elevator_state->direction == arr_query[i]&M_DIRECTION_BIT_MASK){
+            //checks if the moving direction of the query is the same as the elevator
+            if ( arr_query[i] & M_FLOOR_BIT_MASK >= p_elevator_state->floor && p_elevator_state->direction == FSM_DIRECTION_UP)
+            { // if moving up and the floor is higher than the current floor
+            
+                //If the movie is up and the floor is higher than the current floor
+        }
+    }
 
+    
     return 0;
 }
--- utils.h
+++ utils.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "fsm.h"
 
-#define M_QUERY_LEN  10-1
+#define M_QUERY_LEN  10
 
 //Masks for getting correct information from the query info package
 #define M_PRIORITY_BIT_MASK  0x40
@@ -13,8 +13,6 @@
 
 uint8_t sort_query (uint8_t *query, fsm_state_t *elevator_state);
 uint8_t prioritize_inputs (uint8_t *query, fsm_state_t *elevator_state);
-
-
 
 
 
