0beaca5079db7c7822c868cff896c7efdfc8c8bf
Lucas Vo <vogiacnhan@gmail.com>
2025-01-31 16:24:29 +0100

    initial fsm

---
+++ fsm.h
@@ -0,0 +1,79 @@
+#ifndef __FSM_H__
+#define __FSM_H__
+
+#include <stdbool.h> // booleans
+#include <stdint.h>  // xintx_t types
+#include <stdio.h>   // sprintf
+
+
+// Different state of ATM machine
+/// @warning This has to reflect the order of the stateFunctionRow_t array
+typedef enum {
+  FSM_STATE_IDLE,
+  FSM_STATE_UP,
+  FSM_STATE_DOWN,
+  FSM_STATE_ERROR
+} fsm_direction_t;
+
+typedef enum {
+  FSM_STATE_1,
+  FSM_STATE_1_5,
+  FSM_STATE_2,
+  FSM_STATE_2_5,
+  FSM_STATE_3,
+  FSM_STATE_3_5,
+  FSM_STATE_4,
+} fsm_floor_t;
+
+typedef struct fsm_state_s {
+  fsm_direction_t direction;
+  fsm_floor_t floor;
+} fsm_state_t;
+
+// Different type events
+typedef enum {
+  FSM_EVENT_NONE,
+  FSM_EVENT_BOOT,
+  FSM_EVENT_UP,
+  FSM_EVENT_DOWN,
+  FSM_EVENT_IDLE,
+  FSM_EVENT_HALT,
+} fsm_event_t;
+
+
+
+typedef struct stateFunctionRow_s {
+  const char *name;
+  void (*enter_function)(void);
+  void (*update_function)(void); /// continuously while in this state
+  void (*exit_function)(void);
+} stateFunctionRow_t;
+
+typedef struct transitions_s {
+  fsm_state_t currState;
+  fsm_event_t event;
+  fsm_state_t nextState;
+} transitions_t;
+
+
+// Enter functions
+void IdleEnter();
+void UpEnter();
+void DownEnter();
+void ErrorEnter();
+
+// Update functions
+void IdleUpdate();
+void UpUpdate();
+void DownUpdate();
+void ErrorUpdate();
+
+// Exit functions
+void IdleExit();
+void UpExit();
+void DownExit();
+void ErrorExit();
+
+
+
+#endif
---
+++ global.c
@@ -0,0 +1,11 @@
+
+
+
+#include <stdbool.h>
+
+#include "global.h"
+#include "fsm.h"
+
+bool g_floor_panel[6]; /// [4 down, 3 down, 2 down, 3 up, 2 up, 1 up]
+bool g_elevator_panel[4]; /// [1, 2, 3, 4]
+fsm_state_t g_current_state;
