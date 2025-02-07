0941324b3cd588060a70e47c6f64f957ec2f09d6
Vetle Dahl <vetlerd@stud.ntnu.no>
2025-02-07 13:59:46 +0100

    I am evil man

--- source/global.h
+++ source/global.h
@@ -8,14 +8,15 @@
 #define M_QUERY_LEN  10
 
 #define M_FLOOR_BIT_MASK  0x07
-#define M_DIRECTION_BIT_MASK  0x38
+#define M_DIRECTION_BIT_MASK  0x18
+#define M_BUTTON_TYPE_BIT_MASK 0x20
 #define M_PRIORITY_BIT_MASK  0x40
 #define M_ACTIVE_BIT_MASK 0x80
 
 #define M_BUTTON_COUNT 4
 #define M_FLOOR_COUNT 4
 
-typedef uint8_t query_t; // 1 bit for active, 1 bit for priority, 1 bit for button type, bits for direction, 3 bits for floor
+typedef uint8_t query_t; // 1 bit for active, 1 bit for priority, 1 bit for button type, 2 bits for direction, 3 bits for floor
 
 extern bool g_floor_panel[6]; /// [4 down, 3 down, 2 down, 3 up, 2 up, 1 up]
 extern bool g_elevator_panel[4]; /// [1, 2, 3, 4]
